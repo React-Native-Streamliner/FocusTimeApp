3c7731d212065b6b5caab6770d3fff10ce882938
tiger <thisisstreamliner@gmail.com>
2024-04-02 15:11:25 +0300

    refactor: style background

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello worlds</Text>
+      <Text style={styles.text}>Hello worlds</Text>
     </SafeAreaView>
   );
 }
@@ -13,5 +14,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
   },
+  text: {
+    color: colors.white,
+    fontSize: 20,
+    textAlign: 'center',
+  }
 });
