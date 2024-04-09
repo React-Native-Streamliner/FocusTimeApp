ef187bcfd25b3ad8235295aaa6f9129e46b0b041
tiger <thisisstreamliner@gmail.com>
2024-04-09 16:10:53 +0300

    feat: add countdown.js

---
+++ src/components/Countdown.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import { fontSizes, spacing } from "../utils/sizes";
+import { colors } from "../utils/colors";
+
+const minutesToMillis = (min) => min * 1000 * 60;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+export const Countdown = ({ minutes = 0.1, isPaused, onProgress, onEnd }) => {
+    const interval = React.useRef(null);
+
+    const [ millis, setMills ] = useState(null);
+
+    const countdown = () => {
+        setMills((time) => {
+            if (time == 0) {
+                clearInterval(interval.current);
+                onEnd();
+                return time;
+            }
+            const timeLeft = time - 1000;
+            return timeLeft;
+        });
+    };
+
+    useEffect(() => {
+        setMills(minutesToMillis(minutes))
+    }, [minutes]);
+
+    useEffect(() => {
+        onProgress(millis / minutesToMillis(millis));
+    }, [millis]);
+
+    useEffect(() => {
+        if (isPaused) {
+            if (interval.current) clearInterval(interval.current);
+            return;
+        }
+
+        interval.current = setInterval(countdown, 1000);
+        return () => clearInterval(interval.current);
+    }, [isPaused]);
+
+    const minutes = Math.floor(millis / 1000 / 60) % 60;
+    const seconds = Math.floor(millis / 1000) % 60;
+    return (
+        <Text style={styles.text}>
+            {formatTime(minutes)}:{formatTime(seconds)}
+        </Text>
+    )
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: fontSizes.xxxl,
+        fontWeight: 'bold',
+        color: colors.white,
+        padding: spacing.lg,
+        backgroundColor: 'rgba(94, 132, 226, 0.3)',
+    }
+});
