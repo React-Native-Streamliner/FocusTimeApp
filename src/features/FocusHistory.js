4eac7baf819f30c610d03e718e2cbca7a237966d
tiger <thisisstreamliner@gmail.com>
2024-04-19 00:31:18 +0300

    feat: designing focus list

--- src/features/FocusHistory.js
+++ src/features/FocusHistory.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { colors } from "../utils/colors";
+import { fontSizes, spacing } from "../utils/sizes";
 
-const FocusHistory = () => {
+const FocusHistory = ({history}) => {
+    if (!history || !history.length) return null;
+
+    const renderItem = ({item}) => <Text style={styles.item}>- {item}</Text>;
+
     return (
-        <View>
-            <Text>Focus History will be build here</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Things we've focused on:</Text>
+            <FlatList 
+                data={history}
+                renderItem={renderItem}
+            />
         </View>
     )
 }
 
 export default FocusHistory;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: spacing.md,
+        flex: 1,
+    },
+    title: {
+        color: colors.white,
+        fontSize: fontSizes.md,
+        padding: spacing.md,
+        fontWeight: 'bold'
+    },
+    item : {
+        fontSize: fontSizes.md,
+        color: colors.white,
+        paddingTop: spacing.sm,
+    }
+})
